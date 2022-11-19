4e200111fc6fffbd72e98c5af9449e74d3c594e4
dbaric99 <damjana.baric1999@gmail.com>
2022-11-19 22:26:39 +0100

    Finished with options no 2 - Play a match

--- WorldChampionship/Program.cs
+++ WorldChampionship/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 
 //GK, DF, MF, FW - goalkeeper, defender, midfielder, forward
+
+var matchesInLeague = new Dictionary<int, (string Team1, string Team2, int Team1Goals, int Team2Goals, string Winner, int WinnerPoints, string[] PlayersThatScored)>()
+{
+    {1, (Team1: "Belgium", Team2: "Canada", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())},
+    {2, (Team1: "Morocco", Team2: "Croatia", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())},
+    {3, (Team1: "Belgium", Team2: "Morocco", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())},
+    {4, (Team1: "Croatia", Team2: "Canada", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())},
+    {5, (Team1: "Croatia", Team2: "Belgium", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())},
+    {6, (Team1: "Canada", Team2: "Morocco", Team1Goals: 0, Team2Goals: 0, Winner: "", WinnerPoints: 0, PlayersThatScored: Array.Empty<string>())}
+};
+
+int matchInProgress = 0;
 
 var players = assemblePlayersTeam();
 
@@ -18,17 +31,24 @@ do
                 printDividingLine();
                 foreach (var player in players)
                 {
-                    int newRating = trainingSession(player.Value.rating);
-                    Console.WriteLine($"Player: {player.Key} | Position: {player.Value.position} | Rating before: {player.Value.rating} | Rating after practice: {newRating}\n");
-                    players[player.Key] = (position: player.Value.position, rating: newRating);
+                    int newRating = trainingSession(player.Value.Rating);
+                    Console.WriteLine($"Player: {player.Key} | Position: {player.Value.Position} | Rating before: {player.Value.Rating} | Rating after practice: {newRating}\n");
+                    players[player.Key] = (Position: player.Value.Position, Rating: newRating);
                 }
                 printDividingLine();
                 break;
             }
         case 2:
             {
-                var team = formTeam(players);
-                playMatch(team);
+                matchInProgress++;
+                if(matchInProgress > 6)
+                {
+                    Console.WriteLine("All matches from the group are already played!");
+                    break;
+                }
+                Console.WriteLine("MATCH NUM: " + matchInProgress);
+                //TODO: send to method that generates results and then adjust ratings
+                playMatch(players, matchesInLeague, matchInProgress);
                 break;
             }
         case 3:
@@ -60,35 +80,35 @@ Console.ReadKey();
 /* 
  * Used for initialization and return of Dictionary with starting default players 
 */
-Dictionary<string,(string position, int rating)> assemblePlayersTeam()
+Dictionary<string,(string Position, int Rating)> assemblePlayersTeam()
 {
-    return new Dictionary<string, (string position, int rating)>()
+    return new Dictionary<string, (string Position, int Rating)>()
     {
-        {"Luka Modrić", (position: "MF", 88)},
-        {"Marcelo Brozović", (position: "FW", 86)},
-        {"Mateo Kovačić", (position: "DF", 84)},
-        {"Ivan Perišić", (position: "MF", 84)},
-        {"Andrej Kramarić", (position: "MF", 82)},
-        {"Ivan Rakitić", (position: "MF", 82)},
-        {"Joško Gvardiol", (position: "DF",81)},
-        {"Marko Livaja", (position: "FW", 77)},
-        {"Marko Pjaca", (position: "FW", 75)},
-        {"Borna Barišić", (position: "DF", 73)},
-        {"Josip Vuković", (position: "MF", 69)},
-        {"Antonio Marin", (position: "GK", 68)},
-        {"Damjan Đoković", (position: "DF", 70)},
-        {"Domagoj Antolić", (position: "FW", 72)},
-        {"Josip Šutalo", (position: "DF", 75)},
-        {"Ante Budimir", (position: "GK", 76)},
-        {"Mario Pašalić", (position: "MF", 81)},
-        {"Lovro Majer", (position: "FW", 80)},
-        {"Ante Rebić", (position: "DF", 80)},
-        {"Franko Andrijašević", (position: "MF", 72)},
-        {"Fran Tudor", (position: "MF", 72)},
-        {"Dante Stipica", (position: "MF", 71)},
-        {"Ivan Santini", (position: "FW", 69)},
-        {"Dario Melnjak", (position: "GK", 70)},
-        {"Mato Miloš", (position: "FW", 69)}
+        {"Luka Modrić", (Position: "MF", Rating: 88)},
+        {"Marcelo Brozović", (Position: "FW", Rating: 86)},
+        {"Mateo Kovačić", (Position: "DF", Rating: 84)},
+        {"Ivan Perišić", (Position: "MF", Rating: 84)},
+        {"Andrej Kramarić", (Position: "MF", Rating: 82)},
+        {"Ivan Rakitić", (Position: "MF", Rating: 82)},
+        {"Joško Gvardiol", (Position: "DF",Rating: 81)},
+        {"Marko Livaja", (Position: "FW", Rating: 77)},
+        {"Marko Pjaca", (Position: "FW", Rating: 75)},
+        {"Borna Barišić", (Position: "DF", Rating: 73)},
+        {"Josip Vuković", (Position: "MF", Rating: 69)},
+        {"Antonio Marin", (Position: "GK", Rating: 68)},
+        {"Damjan Đoković", (Position: "DF", Rating: 70)},
+        {"Domagoj Antolić", (Position: "FW", Rating: 72)},
+        {"Josip Šutalo", (Position: "DF", Rating: 75)},
+        {"Ante Budimir", (Position: "GK", Rating: 76)},
+        {"Mario Pašalić", (Position: "MF", Rating: 81)},
+        {"Lovro Majer", (Position: "FW", Rating: 80)},
+        {"Ante Rebić", (Position: "DF", Rating: 80)},
+        {"Franko Andrijašević", (Position: "MF", Rating: 72)},
+        {"Fran Tudor", (Position: "MF", Rating: 72)},
+        {"Dante Stipica", (Position: "MF", Rating: 71)},
+        {"Ivan Santini", (Position: "FW", Rating: 69)},
+        {"Dario Melnjak", (Position: "GK", Rating: 70)},
+        {"Mato Miloš", (Position: "FW", Rating: 69)}
     };
 }
 
@@ -131,29 +151,30 @@ int trainingSession(int currentRating)
  * while other players are determined by their rating
  * Returns Dictionary of players that will participate in the game and takes Dictionary of all players as a parameter
 */
-Dictionary<string, (string position, int rating)> formTeam(Dictionary<string, (string position, int rating)> players)
+Dictionary<string, (string Position, int Rating)> formTeam(Dictionary<string, (string Position, int Rating)> players)
 {
-    var team = new Dictionary<string, (string position, int rating)>();
+    var team = new Dictionary<string, (string Position, int Rating)>();
 
     //initializing seperate dictionaries for every position ordered by rating descending
-    var goalkeepers = players.Where(p => p.Value.position == "GK")
-        .OrderByDescending(p => p.Value.rating)
+    var goalkeepers = players.Where(p => p.Value.Position == "GK")
+        .OrderByDescending(p => p.Value.Rating)
         .ToDictionary(p => p.Key, p => p.Value);
 
-    var defenders = players.Where(p => p.Value.position == "DF")
-        .OrderByDescending(p => p.Value.rating)
+    var defenders = players.Where(p => p.Value.Position == "DF")
+        .OrderByDescending(p => p.Value.Rating)
         .ToDictionary(p => p.Key, p => p.Value);
 
-    var midfielders = players.Where(p => p.Value.position == "MF")
-        .OrderByDescending(p => p.Value.rating)
+    var midfielders = players.Where(p => p.Value.Position == "MF")
+        .OrderByDescending(p => p.Value.Rating)
         .ToDictionary(p => p.Key, p => p.Value);
 
-    var strikers = players.Where(p => p.Value.position == "FW")
+    var strikers = players.Where(p => p.Value.Position == "FW")
         .ToDictionary(p => p.Key, p => p.Value);
+
+    //TODO: handle if there are not enough players to form a team
 
     //adding all the players to the team
     team.Add(goalkeepers.ElementAt(0).Key, goalkeepers.ElementAt(0).Value);
-    listPlayers(team);
 
     team.Add(defenders.ElementAt(0).Key, defenders.ElementAt(0).Value);
     team.Add(defenders.ElementAt(1).Key, defenders.ElementAt(1).Value);
@@ -164,15 +185,35 @@ Dictionary<string, (string position, int rating)> formTeam(Dictionary<string, (s
     team.Add(midfielders.ElementAt(1).Key, midfielders.ElementAt(1).Value);
     team.Add(midfielders.ElementAt(2).Key, midfielders.ElementAt(2).Value);
 
-    Random rand = new Random();
-    int indexToAdd = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        indexToAdd = rand.Next(0, strikers.Count);
-        team.Add(strikers.ElementAt(indexToAdd).Key, strikers.ElementAt(indexToAdd).Value);
-    }
+    var rand = new Random();
+    var randomIndices = Enumerable.Range(0, strikers.Count).OrderBy(x => rand.Next()).Take(3).ToList();
+
+    
+    foreach (var ind in randomIndices)
+        team.Add(strikers.ElementAt(ind).Key, strikers.ElementAt(ind).Value);
 
     return team;
+}
+
+/*
+ * Increases a players rating in the players Dictionary
+ * Returns a new updated dictionary and takes players name and percentage by which a rating should increase
+*/
+Dictionary<string, (string Position, int Rating)> changePlayerRating(Dictionary<string, (string Position, int Rating)> players, string playerName, int percentageNumber, bool shouldIncrease)
+{
+    //TODO: could use one method to training and increasing the rating after the game
+    foreach (var player in players)
+    {
+        if (player.Key == playerName)
+        {
+            decimal percentage = ((decimal)percentageNumber) / 100;
+            int newRating = shouldIncrease ?
+                (int)(Math.Round(player.Value.Rating + player.Value.Rating * percentage, MidpointRounding.AwayFromZero))
+                : (int)(Math.Round(player.Value.Rating - player.Value.Rating * percentage, MidpointRounding.AwayFromZero));
+            players[player.Key] = (Position: player.Value.Position, Rating: newRating);
+        }
+    }
+    return players;
 }
 
 /*
@@ -180,20 +221,67 @@ Dictionary<string, (string position, int rating)> formTeam(Dictionary<string, (s
  * Print out the result and save it
  * Max number of matches that can be played are 6
 */
-void playMatch(Dictionary<string, (string position, int rating)> matchPlayers)
+(string Team1, string Team2, int Team1Goals, int Team2Goals, string Winner, int WinnerPoints, string[] PlayersThatScored) playMatch(Dictionary<string, (string Position, int Rating)> allPlayers, Dictionary<int, (string Team1, string Team2, int Team1Goals, int Team2Goals, string Winner, int WinnerPoints, string[] PlayersThatScored)> matches, int numberOfMatch)
 {
+    //calls function that forms the team of players that are playing the current match
+    var team = formTeam(allPlayers);
 
+    //TODO: check if team has all the players if it doesn't return null
+
+    var strikers = allPlayers.Where(p => p.Value.Position == "FW");
+    var restOfPlayers = allPlayers.Except(strikers);
+
+    var matchInProgress = matches[numberOfMatch];
+
+    //generate goals for both teams randomly
+    var rand = new Random();
+    int team1Goals = rand.Next(0, 5);
+    int team2Goals = rand.Next(0, 5);
+    while (team1Goals == team2Goals)
+        team2Goals = rand.Next(0, 5);
+
+    //calculate match winner and the amount of points they get
+    string winner = (team1Goals > team2Goals) ? matchInProgress.Team1 : matchInProgress.Team2;
+    int winnerPoints = (winner == matchInProgress.Team1) ? team1Goals : team2Goals;
+
+    //generate players that scored the goals for croatia
+    string[] playersThatScored = Array.Empty<string>();
+    if (new string[2] {matchInProgress.Team1, matchInProgress.Team2}.Contains("Croatia"))
+    {
+        var croatiaGoals = (matchInProgress.Team1 == "Croatia") ? team1Goals : team2Goals;
+        playersThatScored = new string[croatiaGoals];
+
+        if(croatiaGoals > 0)
+        {
+            var rnd = new Random();
+            int strikerInd = 0;
+            for (int i = 0; i < croatiaGoals; i++)
+            {
+                strikerInd = rnd.Next(0, croatiaGoals);
+                playersThatScored[i] = strikers.ElementAt(strikerInd).Key;
+            }
+        }
+
+        //change player's ratings accordingly
+        foreach (var striker in playersThatScored)
+            changePlayerRating(allPlayers, striker, 5, true);
+
+        foreach (var player in restOfPlayers)
+            changePlayerRating(allPlayers, player.Key, 2, (winner == "Croatia") ? true : false);
+    }
+
+    return (Team1: matchInProgress.Team1, Team2: matchInProgress.Team2, Team1Goals: team1Goals, Team2Goals: team2Goals, Winner: winner, WinnerPoints: winnerPoints, PlayersThatScored: playersThatScored);
 }
 
 /*
  * Prints out all players from the Dictionary
 */
-void listPlayers(Dictionary<string, (string position, int rating)> listOfPlayers)
+void listPlayers(Dictionary<string, (string Position, int Rating)> listOfPlayers)
 {
     printDividingLine();
     foreach (var player in listOfPlayers)
     {
-        Console.WriteLine($"Player: {player.Key} | Position: {player.Value.position} | Rating: {player.Value.rating}\n");
+        Console.WriteLine($"Player: {player.Key} | Position: {player.Value.Position} | Rating: {player.Value.Rating}\n");
     }
     printDividingLine();
 }
